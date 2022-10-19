d5bde11848dac598c67ae7727ad9d0357ed8c9c6
Renan Hiroki Bastos <hirokibastos@gmail.com>
2022-10-18 21:55:54 -0300

    [ADD] Add PixController and PixService

---
+++ carteirinhaBack/src/main/java/com.unicamp/controller/PixController.java
@@ -0,0 +1,32 @@
+package com.unicamp.controller;
+
+import com.unicamp.service.PixService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class PixController {
+
+    // Show QR Code on browser
+    @PostMapping(value="/generate-pix")
+    public void generatePix(HttpServletResponse response
+            , @RequestParam String cpf
+            , @RequestParam String nome
+            , @RequestParam String valor
+    ) throws IOException {
+        String json = PixService.generatePix(cpf, nome, valor);
+        response.getWriter().write(json);
+    }
+
+    // Show QR Code on browser
+    @GetMapping(value="/consult-pix")
+    public void consultPix(HttpServletResponse response, @RequestParam String txid) throws IOException {
+        String json = PixService.consultPix(txid);
+        response.getWriter().write(json);
+    }
+
+}
---
+++ carteirinhaBack/src/main/java/com.unicamp/service/PixService.java
@@ -0,0 +1,80 @@
+package com.unicamp.service;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class PixService {
+
+    private static String basic_auth_string = "Basic ZXlKcFpDSTZJbUVpTENKamIyUnBaMjlRZFdKc2FXTmhaRzl5SWpvd0xDSmpiMlJwWjI5VGIyWjBkMkZ5WlNJNk5EWXhNelFzSW5ObGNYVmxibU5wWVd4SmJuTjBZV3hoWTJGdklqb3hmUTpleUpwWkNJNklqTTBObUZtTWpjdFltRTJaaTAwTlRFNExUbGtZeUlzSW1OdlpHbG5iMUIxWW14cFkyRmtiM0lpT2pBc0ltTnZaR2xuYjFOdlpuUjNZWEpsSWpvME5qRXpOQ3dpYzJWeGRXVnVZMmxoYkVsdWMzUmhiR0ZqWVc4aU9qRXNJbk5sY1hWbGJtTnBZV3hEY21Wa1pXNWphV0ZzSWpveExDSmhiV0pwWlc1MFpTSTZJbWh2Ylc5c2IyZGhZMkZ2SWl3aWFXRjBJam94TmpZMU5qZ3pOek16TVRNeGZR";
+    private static String dev_app_key = "d27b977908ffab20136ae17d70050f56b971a5bb";
+
+    private static String generateAuthToken() {
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+            RequestBody body = RequestBody.create("grant_type=client_credentials&scope=cob.read cob.write pix.read pix.write", mediaType);
+            Request request = new Request.Builder()
+                    .url("https://oauth.hm.bb.com.br/oauth/token")
+                    .method("POST", body)
+                    .addHeader("Authorization", basic_auth_string)
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .build();
+            Response response = client.newCall(request).execute();
+            JSONObject json = new JSONObject(response.body().string());
+            return json.getString("access_token");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String generatePix(String cpf, String nome, String valor) {
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create("{\n    \"calendario\": {\n        \"expiracao\": 300\n    },\n    \"devedor\": {\n        \"cpf\": " + cpf + ",\n        \"nome\": \"" + nome + "\"\n    },\n    \"valor\": {\n        \"original\": " + valor + "\n    },\n    \"chave\": \"28779295827\",\n    \"solicitacaoPagador\": \"Recarga de RA.\"\n}", mediaType);
+            Request request = new Request.Builder()
+                    .url("https://api.hm.bb.com.br/pix/v1/cobqrcode/" + PixService.generateTxid(cpf, nome) + "?gw-dev-app-key=" + dev_app_key)
+                    .method("PUT", body)
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Authorization", "Bearer " + PixService.generateAuthToken())
+                    .build();
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String consultPix(String txid) {
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("text/plain");
+            RequestBody body = RequestBody.create(mediaType, "");
+            Request request = new Request.Builder()
+                    .url("https://api.hm.bb.com.br/pix/v1/cob/" + txid + "?gw-dev-app-key=" + dev_app_key)
+                    .addHeader("Authorization", "Bearer " + PixService.generateAuthToken())
+                    .build();
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String generateTxid(String cpf, String nome){
+        nome = nome.replace("-", "").toUpperCase();
+        return nome + cpf + "000001";
+    }
+
+}
