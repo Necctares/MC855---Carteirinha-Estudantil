3ba861afadd1db8f73906729b45fb8bfe3ecda81
Renan Hiroki Bastos <hirokibastos@gmail.com>
2022-11-01 17:21:32 -0300

    Added mysql dependency

--- carteirinhaBack/src/main/java/com/unicamp/App.java
+++ carteirinhaBack/src/main/java/com/unicamp/App.java
@@ -2,8 +2,10 @@ package com.unicamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// TODO: Pesquisar sobre essa exclusão
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
