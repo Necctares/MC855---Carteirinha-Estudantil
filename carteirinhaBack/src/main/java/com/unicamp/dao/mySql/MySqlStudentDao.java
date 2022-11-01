3fdd483a9325f2e4a499184d9d1c56636647b9f1
Necctares <alanxribeiro98@gmail.com>
2022-11-01 17:26:11 -0300

    Implementado mysqlstudentdao

--- carteirinhaBack/src/main/java/com/unicamp/dao/mySql/MySqlStudentDao.java
+++ carteirinhaBack/src/main/java/com/unicamp/dao/mySql/MySqlStudentDao.java
@@ -2,32 +2,33 @@ package com.unicamp.dao.mySql;
 
 import java.util.Collection;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 import com.unicamp.dao.StudentDao;
 import com.unicamp.entity.Student;
 
-// TODO Implement MySql DAO
+@Repository
+@Qualifier("carteirinha")
 public class MySqlStudentDao implements StudentDao {
     private static Map<Integer, Student> students;
 
+    @Override
     public Collection<Student> getAllStudents() {
         return this.students.values();
     }
 
     @Override
     public Student getStudentById(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+        return this.students.get(id);
     }
 
     @Override
     public void removeStudentById(Integer id) {
-        // TODO Auto-generated method stub
-        
+        this.students.remove(id);
     }
 
     @Override
     public void insertStudentToDb(Student student) {
-        // TODO Auto-generated method stub
-        
+        this.students.put(student.getRa(), student);
     }
 }
